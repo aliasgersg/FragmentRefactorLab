ee6bdb59892ed2800eb0cc6b1a6571272008cf7d
aliasgersg <alighotawala02@gmail.com>
2023-10-25 15:43:05 -0400

    In this commit I created a fragment file and layout for an app that asks your name and outputs it

---
+++ app/src/main/java/edu/temple/fragmentrefactor/NameFragment.kt
@@ -0,0 +1,35 @@
+package edu.temple.fragmentrefactor
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+
+class NameFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val rootView = inflater.inflate(R.layout.fragment_name, container, false)
+
+        val displayTextView = rootView.findViewById<TextView>(R.id.displayTextView)
+        val nameEditText = rootView.findViewById<EditText>(R.id.nameEditText)
+        val changeButton = rootView.findViewById<Button>(R.id.changeButton)
+
+        changeButton.setOnClickListener {
+            val name = nameEditText.text.toString()
+
+            displayTextView.text = if (name.isNotBlank()) {
+                "Hello, $name!"
+            } else {
+                "Please enter your name"
+            }
+        }
+
+        return rootView
+    }
+}
